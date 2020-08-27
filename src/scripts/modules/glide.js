dcc2c7c2721021e39d8501eb60c4aea7db1a62ef
Rob <rob@kbamarketing.co.uk>
2020-08-27 12:52:57 +0100

    Start adding the next slider

--- src/scripts/modules/glide.js
+++ src/scripts/modules/glide.js
@@ -3,3 +3,4 @@
 import Glide from '../tools/glide';
 
 new Glide('#hero-js', {type: 'slider', startAt: 0, perView: 1, focusAt: 'left'}).mount();
+new Glide('.glidejs-sl-1', {type: 'slider', startAt: 0, perView: 4, focusAt: 'left'}).mount();
